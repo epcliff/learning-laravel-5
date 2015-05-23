84111fa89eec31a2082a44f34bfa78c3618700ed
Ep Talampas <epcliff@yahoo.com>
2015-05-24 01:50:48 +0800

    #The Service Container

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,23 @@
 <?php
 
+use App\Http\Controllers\FooController;
+
+Route::get('foo', 'FooController@foo');
+
+//interface BarInterface {}
+
+//class Bar implements BarInterface {}
+//class SecondBar implements BarInterface {}
+//
+//App::bind('BarInterface', 'SecondBar');
+//
+//Route::get('bar', function(BarInterface $bar) {
+//	dd($bar);
+//});
+//
+//Route::get('/', function() {
+//	return 'Home Page';
+//});
 //Route::get('about', 'PagesController@about');
 //Route::get('about', ['middleware' => 'auth', 'uses' => 'PagesController@about']);
 Route::get('about', ['middleware' => 'auth', function()
@@ -7,10 +25,10 @@ Route::get('about', ['middleware' => 'auth', function()
 	return 'this page will only show if the user is signed in';
 }]);
 
-Route::get('foo',['middleware' => 'manager', function()
-{
-	return 'this page will only be viewed by a manager';
-}]);
+//Route::get('foo',['middleware' => 'manager', function()
+//{
+//	return 'this page will only be viewed by a manager';
+//}]);
 
 Route::get('contact', 'PagesController@contact');
 
